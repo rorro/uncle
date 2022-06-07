7f309f355a1a2a7d574e9173c1391278786cb8ec
Robin <rorrosliwa@gmail.com>
2022-06-07 19:47:23 +0200

    Add pet command

--- src/commands.ts
+++ src/commands.ts
@@ -3,10 +3,12 @@ import { helpCommand } from './commands/Help';
 import { checkApplicantRequirementsCommand } from './commands/CheckApplicantRequirements';
 import { acceptApplicationCommand } from './commands/AcceptApplication';
 import { messageCommand } from './commands/Message';
+import { petsCommand } from './commands/Pets';
 
 export const commands: Command[] = [
   helpCommand,
   checkApplicantRequirementsCommand,
   acceptApplicationCommand,
-  messageCommand
+  messageCommand,
+  petsCommand
 ];

e28cea3ce0993e42bca09a8cccceb05d1b1a1445
Robin <5983417+rorro@users.noreply.github.com>
2022-12-15 14:19:56 +0100

    Update to Wise Old Man API v2

--- src/api/handler.ts
+++ src/api/handler.ts
@@ -1,10 +1,10 @@
+import { WOMClient } from '@wise-old-man/utils';
 import axios from 'axios';
 import config from '../config';
 
-export const WOMAPI = axios.create({
-  baseURL: config.wiseoldmanAPI,
-  timeout: 60000,
-  headers: { 'User-Agent': 'Uncle Discord Bot' }
+export const womClient = new WOMClient({
+  apiKey: process.env.WOM_API_KEY,
+  userAgent: 'Uncle Discord Bot'
 });
 
 export const RWAPI = axios.create({

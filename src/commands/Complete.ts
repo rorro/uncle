603fcce3a233a5f1edac1760d39df3bf8aad3074
Robin <5983417+rorro@users.noreply.github.com>
2023-04-26 15:44:19 +0200

    Add complete command (#44)

---
+++ src/commands/Complete.ts
@@ -0,0 +1,76 @@
+import { ChatInputCommandInteraction, Client, ApplicationCommandType, ApplicationCommandOptionType } from 'discord.js';
+import { Command } from '../types';
+import { combinations } from '../utils';
+
+export const completeCommand: Command = {
+  name: 'complete',
+  description: 'Calculate the amount of rolls needed to complete a set of items',
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+        name: 'drop_rates',
+        description: `Format: 1/50 3;1/300`,
+        type: ApplicationCommandOptionType.String,
+        required: true
+    },
+    {
+        name: 'decimals',
+        description: 'The amount of decimal points to round to.',
+        type: ApplicationCommandOptionType.Number,
+    }
+  ],
+  run: async (client: Client, interaction: ChatInputCommandInteraction) => {
+    const input = interaction.options.getString('drop_rates', true);
+
+    const fractionRegex = new RegExp("^(?:\\d+\\/\\d+|\\d+;\\d+\\/\\d+)(?:\\s+(?:\\d+\\/\\d+|\\d+;\\d+\\/\\d+))*$");
+    if (!fractionRegex.test(input)) {
+        await interaction.reply({
+              content: `The given format was wrong. Please use the following format: amount;numerator/denominator. For example 3;1/150 1/500 5;1/100.`,
+              ephemeral: true
+        });
+        return;
+    }
+
+    const decimals = interaction.options.getNumber('decimals');
+    const roundTo = decimals ? decimals : 0;
+
+    let avg = 0;
+    let sign = 1;
+    const probs = parseInput(input);
+
+    for (let i = 0; i < probs.length; i++) {
+        let innerSum = 0;
+        const combs = combinations(probs, i+1);
+
+        for (const c of combs) {
+            innerSum += 1/(c.reduce((sum, curr) => sum + curr, 0))
+        }
+        avg += sign * innerSum;
+        sign *= -1
+    }
+    
+    await interaction.reply({
+      content: `${avg.toFixed(roundTo)} rolls needed on average to get all items.`,
+      ephemeral: true
+    });
+  }
+};
+
+function parseInput(input: string): number[] {
+    let result: number[] = []
+    const splitInput = input.split(' ');
+    
+    for (const item of splitInput) {
+        let amount, rate = ''
+        if (item.includes(';')) {
+            [amount, rate] = item.split(';');
+        } else {
+            rate = item;
+        }
+
+        const [numerator, denominator] = rate.split('/').map(x => parseInt(x));
+        result.push(...new Array(amount ? parseInt(amount) : 1).fill(numerator / denominator))
+    }
+
+    return result;
+}
--- src/utils.ts
+++ src/utils.ts
@@ -157,3 +157,24 @@ export function getRoleName(roleId: string): string {
   const roles = config.guild.roles;
   return roleId === roles.protector ? 'Protector' : roleId === roles.bulwark ? 'Bulwark' : 'Justiciar';
 }
+
+
+// Code shamelessly stolen from: https://stackoverflow.com/a/54385026
+function* range(start: number, end: number) {
+  for (; start <= end; ++start) { yield start; }
+}
+
+function last<T>(arr: T[]) { return arr[arr.length - 1]; }
+
+function* numericCombinations(n: number, r: number, loc: number[] = []): IterableIterator<number[]> {
+  const idx = loc.length;
+  if (idx === r) {
+    yield loc;
+    return;
+  }
+  for (let next of range(idx ? last(loc) + 1 : 0, n - r + idx)) { yield* numericCombinations(n, r, loc.concat(next)); }
+}
+
+export function* combinations<T>(arr: T[], r: number) {
+  for (let idxs of numericCombinations(arr.length, r)) { yield idxs.map(i => arr[i]); }
+}

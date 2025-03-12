0cdf8193c2003c012f4e56638853c73f87a55a22
Robin <5983417+rorro@users.noreply.github.com>
2025-03-12 02:09:57 +0100

    Defer update on split action and disable buttons

--- src/interactions/splits.ts
+++ src/interactions/splits.ts
@@ -1,4 +1,14 @@
-import { ButtonInteraction, EmbedBuilder, MessageFlags } from 'discord.js';
+import {
+  ActionRow,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonComponent,
+  ButtonInteraction,
+  ComponentType,
+  EmbedBuilder,
+  MessageActionRowComponent,
+  MessageFlags
+} from 'discord.js';
 import { getSheetData, insertIntoSheet } from '../api/googleHandler';
 import config from '../config';
 import { isStaff } from '../utils';
@@ -14,10 +24,13 @@ export async function saveSplit(interaction: ButtonInteraction) {
     return;
   }
 
-  const toWrite = JSON.parse(interaction.message.content.replaceAll('||', '').slice(2));
-  const embed = new EmbedBuilder(interaction.message.embeds[0].toJSON());
+  const originalRow = interaction.message.components[0];
+  await interaction.update({ components: [disableButtons(originalRow, true)] });
 
   try {
+    const toWrite = JSON.parse(interaction.message.content.replaceAll('||', '').slice(2));
+    const embed = new EmbedBuilder(interaction.message.embeds[0].toJSON());
+
     const currentData = await getSheetData(
       config.googleDrive.newSplitsSheet,
       'Bot splits dump!A1:A',
@@ -27,12 +40,16 @@ export async function saveSplit(interaction: ButtonInteraction) {
     await insertIntoSheet(config.googleDrive.newSplitsSheet, `Bot splits dump!A${index + 1}`, [toWrite]);
 
     embed.setFooter({ text: `Approved by: ${interaction.user.displayName}` });
-    await interaction.update({ content: '', embeds: [embed], components: [] });
+    await interaction.editReply({ content: '', embeds: [embed], components: [] });
   } catch (e) {
-    await interaction.reply({
+    console.log(`ERROR approving split, ${e}`);
+
+    await interaction.editReply({ components: [disableButtons(originalRow, false)] });
+    await interaction.followUp({
       content: `Something went wrong, show this to Rorro ${e}`,
       flags: MessageFlags.Ephemeral
     });
+    return;
   }
 }
 
@@ -40,22 +57,44 @@ export async function denySplit(interaction: ButtonInteraction) {
   if (!interaction.inCachedGuild()) return;
 
   if (!isStaff(interaction.member)) {
-    await interaction.reply({
+    await interaction.followUp({
       content: 'You need to be a staff member to use this button.',
       flags: MessageFlags.Ephemeral
     });
     return;
   }
 
-  const embed = new EmbedBuilder(interaction.message.embeds[0].toJSON());
+  const originalRow = interaction.message.components[0];
+  await interaction.update({ components: [disableButtons(originalRow, true)] });
 
   try {
+    const embed = new EmbedBuilder(interaction.message.embeds[0].toJSON());
     embed.setFooter({ text: `Denied by: ${interaction.user.displayName}` }).setColor('DarkRed');
-    await interaction.update({ content: '', embeds: [embed], components: [] });
+
+    await interaction.editReply({ content: '', embeds: [embed], components: [] });
   } catch (e) {
-    await interaction.reply({
+    console.log(`ERROR denying split, ${e}`);
+    await interaction.editReply({ components: [disableButtons(originalRow, false)] });
+
+    await interaction.followUp({
       content: `Something went wrong, show this to Rorro ${e}`,
       flags: MessageFlags.Ephemeral
     });
+    return;
   }
+}
+
+function disableButtons(
+  originalRow: ActionRow<MessageActionRowComponent>,
+  disable: boolean
+): ActionRowBuilder<ButtonBuilder> {
+  const newRow = new ActionRowBuilder<ButtonBuilder>();
+
+  originalRow.components.forEach(button => {
+    if (button.type !== ComponentType.Button) return;
+
+    newRow.addComponents(ButtonBuilder.from(button as ButtonComponent).setDisabled(disable));
+  });
+
+  return newRow;
 }
